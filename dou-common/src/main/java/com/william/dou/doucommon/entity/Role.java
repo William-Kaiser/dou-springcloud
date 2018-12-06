0a521986385b533332f5857e5ae6dbcda628c3da
William-Kaiser <712677107@qq.com>
2018-12-06 14:12:36 +0800

    first commit

---
+++ dou-common/src/main/java/com/william/dou/doucommon/entity/Role.java
@@ -0,0 +1,45 @@
+package com.william.dou.doucommon.entity;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author likai
+ */
+public class Role implements Serializable {
+
+    private Integer id;
+
+    private String roleName;
+    /**
+     *一个角色对应多个用户
+     */
+    private List<User> userList;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+}
+
---
+++ dou-common/src/main/java/com/william/dou/doucommon/entity/User.java
@@ -0,0 +1,60 @@
+package com.william.dou.doucommon.entity;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author likai
+ */
+public class User implements Serializable {
+
+    private Integer userId;
+    /**
+     *   用户姓名
+     */
+    private String userName;
+    /**
+     *   密码
+     */
+    private String password;
+
+    /**
+     *   一个用户具有多个角色
+     */
+    private List<Role> roleList;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public List<Role> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<Role> roleList) {
+        this.roleList = roleList;
+    }
+}
+
