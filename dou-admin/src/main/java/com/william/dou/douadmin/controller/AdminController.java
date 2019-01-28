9c5259a5e3d42eb96d4a9ccc3071e626487b9abb
William-Kaiser <712677107@qq.com>
2019-01-28 14:45:24 +0800

    **************

--- dou-admin/src/main/java/com/william/dou/douadmin/controller/AdminController.java
+++ dou-admin/src/main/java/com/william/dou/douadmin/controller/AdminController.java
@@ -4,7 +4,6 @@ import com.william.dou.common.base.BaseController;
 import com.william.dou.common.entity.User;
 import com.william.dou.douadmin.feign.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
